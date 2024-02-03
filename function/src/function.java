fde329938ae3d19e5c7ffe6df8cc61c676d37821
Phyo <phyoheinko.phk1@gmail.com>
2024-02-03 15:55:46 +0630

    function/src/function.java

--- function/src/function.java
+++ function/src/function.java
@@ -4,16 +4,25 @@ import java.io.IOException;
 
 
 public class function {
-    static File musicFile = new File("/home/phyo/Downloads/StarWars3.wav");
+    //music file location
+    static File musicFile = new File("/file-location/music-name.wav");
     public static void main(String[] args) throws UnsupportedAudioFileException, IOException, LineUnavailableException, InterruptedException{
 
-        
+        //get music file frome file location
         AudioInputStream audioOut = AudioSystem.getAudioInputStream(musicFile);
+
+        //decode the music (but only wav work)
         Clip clip = AudioSystem.getClip();
         clip.open(audioOut);
-        System.out.println(clip.getMicrosecondLength()/1000000);
+
+        //music length in seconds
+        System.out.println(clip.getMicrosecondLength()/1000000 + "s");
+        
+        //start the music
         clip.start();
         int gg = 0;
+
+        //this loop is for continuation of music because computer process the program faster than human
         while (gg==0) {
             Thread.sleep(clip.getMicrosecondLength());
             gg=1;
