25ad6196f7a76e7196f83f1aa9a6a2425009e9a8
Replit user <>
2024-02-04 02:58:48 +0000

    integration/mp3decoder.java

---
+++ integration/mp3decoder.java
@@ -0,0 +1,21 @@
+import java.io.File;
+import java.io.IOException;
+import javax.sound.sampled.*;
+
+public class mp3decoder {
+  public static void main(String[] args) throws UnsupportedAudioFileException, IOException, LineUnavailableException{
+    File file = new File("integration/Sample Song/Amazing.mp3");
+  AudioInputStream mp3in = AudioSystem.getAudioInputStream(file);
+  AudioInputStream mp3d = null;
+  AudioFormat format = mp3in.getFormat();
+  AudioFormat decodedFormat = new AudioFormat(AudioFormat.Encoding.PCM_SIGNED,format.getSampleRate(),16,format.getChannels(),format.getChannels()*2,format.getSampleRate(),false);
+  mp3d = AudioSystem.getAudioInputStream(decodedFormat,mp3in);
+  Clip clip = AudioSystem.getClip();
+  clip.open(mp3d);
+  clip.start();
+boolean flag = true;
+while(flag){
+  System.out.print(".");
+}
+  }
+}
